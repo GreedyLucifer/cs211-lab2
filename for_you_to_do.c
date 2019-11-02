1b896022da46a261fc7fe59525900380a1f399cd
GreedyLucifer <46939658+GreedyLucifer@users.noreply.github.com>
2019-11-01 17:45:14 -0700

    Add files via upload

--- for_you_to_do.c
+++ for_you_to_do.c
@@ -19,9 +19,9 @@
 int mydgetrf(double *A, int *ipiv, int n) 
 {
     /* add your code here */
-	int i, t, j, k, maxind, temps;
-	double max;
-	double *tempv = (double*)malloc(sizeof(double) * n);
+	register int i, t, j, k, maxind, temps;
+	register double max;
+	register double *tempv = (double*)malloc(sizeof(double) * n);
 	for (i = 0; i < (n - 1); i++)
 	{
 		// pivoting
@@ -100,15 +100,15 @@ int mydgetrf(double *A, int *ipiv, int n)
 void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
 {
     /* add your code here */
-	int i, j;
+	register int i, j;
 	register double sum;
-	double *y = (double*)malloc(n * sizeof(double));
+	register double *y = (double*)malloc(n * sizeof(double));
 	if (UPLO == 'L')
 	{
 		y[0] = B[ipiv[0]];
 		for (i = 1; i < n; i++)
 		{
-			sum = 0.0;
+			sum = 0;
 			for (j = 0; j < i; j++)
 			{
 				sum += y[j] * A[i*n + j];
@@ -140,74 +140,98 @@ void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
  * Same function as what you used in lab1, cache_part4.c : optimal( ... ).
  * 
  **/
-void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
+void mydgemm(const double *A, const double *B, double *C, int n, int i, int j, int k, int b)
 {
-    /* add your code here */
-    /* please just copy from your lab1 function optimal( ... ) */
+	/* add your code here */
+	/* please just copy from your lab1 function optimal( ... ) */
 	int i1, j1, k1;
-	/* B x B mini matrix multiplications */
-	for (i1 = i; (i1 < i + b) && (i1 < n); i1 += 3)
-		for (j1 = j; (j1 < j + b) && (j1 < n); j1 += 3)
-		{
-			int c0 = i1 * n + j1;
-			int c1 = c0 + n;
-			int c2 = c1 + n;
-			register double c00 = C[c0];
-			register double c01 = C[c0 + 1];
-			register double c02 = C[c0 + 2];
-			register double c10 = C[c1];
-			register double c11 = C[c1 + 1];
-			register double c12 = C[c1 + 2];
-			register double c20 = C[c2];
-			register double c21 = C[c2 + 1];
-			register double c22 = C[c2 + 2];
-
-			for (k1 = k; (k1 < k + b) && (k1 < n); k1 += 3)
+		/* B x B mini matrix multiplications */
+		for (i1 = i; (i1 < i + b) && (i1 < n); i1 += 4)
+			for (j1 = j; (j1 < j + b) && (j1 < n); j1 += 4)
 			{
-				int a0 = i1 * n + k1;
-				int a1 = a0 + n;
-				int a2 = a1 + n;
-				int b0 = k1 * n + j1;
-				int b1 = b0 + n;
-				int b2 = b1 + n;
-				register double a00 = A[a0];
-				register double a10 = A[a1];
-				register double a20 = A[a2];
-				register double b00 = B[b0]; register double b01 = B[b0 + 1]; register double b02 = B[b0 + 2];
+				int c0 = i1 * n + j1;
+				int c1 = c0 + n;
+				int c2 = c1 + n;
+				int c3 = c2 + n;
+				register double c00 = C[c0];
+				register double c01 = C[c0 + 1];
+				register double c02 = C[c0 + 2];
+				register double c03 = C[c0 + 3];
+				register double c10 = C[c1];
+				register double c11 = C[c1 + 1];
+				register double c12 = C[c1 + 2];
+				register double c02 = C[c1 + 3];
+				register double c20 = C[c2];
+				register double c21 = C[c2 + 1];
+				register double c22 = C[c2 + 2];
+				register double c23 = C[c2 + 3];
+				register double c30 = C[c3];
+				register double c31 = C[c3 + 1];
+				register double c32 = C[c3 + 2];
+				register double c33 = C[c3 + 3];
 
-				c00 += a00 * b00; c01 += a00 * b01; c02 += a00 * b02;
-				c10 += a10 * b00; c11 += a10 * b01; c12 += a10 * b02;
-				c20 += a20 * b00; c21 += a20 * b01; c22 += a20 * b02;
+				for (k1 = k; (k1 < k + b) && (k1 < n); k1 += 4)
+				{
+					int a0 = i1 * n + k1;
+					int a1 = a0 + n;
+					int a2 = a1 + n;
+					int a3 = a2 + n;
+					int b0 = k1 * n + j1;
+					int b1 = b0 + n;
+					int b2 = b1 + n;
+					int b3 = b2 + n;
+					double a00 = A[a0];
+					double a10 = A[a1];
+					double a20 = A[a2];
+					double a30 = A[a3];
+					double b00 = B[b0]; double b01 = B[b0 + 1]; double b02 = B[b0 + 2]; double b03 = B[b0 + 3];
 
-				a00 = A[a0 + 1];
-				a10 = A[a1 + 1];
-				a20 = A[a2 + 1];
-				b00 = B[b1]; b01 = B[b1 + 1]; b02 = B[b1 + 2];
+					c00 += a00 * b00; c01 += a00 * b01; c02 += a00 * b02; c03 += a00 * b03;
+					c10 += a10 * b00; c11 += a10 * b01; c12 += a10 * b02; c13 += a10 * b03;
+					c20 += a20 * b00; c21 += a20 * b01; c22 += a20 * b02; c23 += a20 * b03;
+					c30 += a30 * b00; c31 += a30 * b01; c32 += a30 * b02; c33 += a30 * b03;
 
-				c00 += a00 * b00; c01 += a00 * b01; c02 += a00 * b02;
-				c10 += a10 * b00; c11 += a10 * b01; c12 += a10 * b02;
-				c20 += a20 * b00; c21 += a20 * b01; c22 += a20 * b02;
+					a00 = A[a0 + 1];
+					a10 = A[a1 + 1];
+					a20 = A[a2 + 1];
+					a30 = A[a3 + 1];
+					b00 = B[b1]; b01 = B[b1 + 1]; b02 = B[b1 + 2]; b03 = B[b1 + 3];
 
-				a00 = A[a0 + 2];
-				a10 = A[a1 + 2];
-				a20 = A[a2 + 2];
-				b00 = B[b2]; b01 = B[b2 + 1]; b02 = B[b2 + 2];
+					c00 += a00 * b00; c01 += a00 * b01; c02 += a00 * b02; c03 += a00 * b03;
+					c10 += a10 * b00; c11 += a10 * b01; c12 += a10 * b02; c13 += a10 * b03;
+					c20 += a20 * b00; c21 += a20 * b01; c22 += a20 * b02; c23 += a20 * b03;
+					c30 += a30 * b00; c31 += a30 * b01; c32 += a30 * b02; c33 += a30 * b03;
 
-				c00 += a00 * b00; c01 += a00 * b01; c02 += a00 * b02;
-				c10 += a10 * b00; c11 += a10 * b01; c12 += a10 * b02;
-				c20 += a20 * b00; c21 += a20 * b01; c22 += a20 * b02;
+					a00 = A[a0 + 2];
+					a10 = A[a1 + 2];
+					a20 = A[a2 + 2];
+					a30 = A[a3 + 2];
+					b00 = B[b2]; b01 = B[b2 + 1]; b02 = B[b2 + 2]; b03 = B[b2 + 3];
 
+					c00 += a00 * b00; c01 += a00 * b01; c02 += a00 * b02; c03 += a00 * b03;
+					c10 += a10 * b00; c11 += a10 * b01; c12 += a10 * b02; c13 += a10 * b03;
+					c20 += a20 * b00; c21 += a20 * b01; c22 += a20 * b02; c23 += a20 * b03;
+					c30 += a30 * b00; c31 += a30 * b01; c32 += a30 * b02; c33 += a30 * b03;
+
+				}
+				C[c0] = c00;
+				C[c0 + 1] = c01;
+				C[c0 + 2] = c02;
+				C[c0 + 3] = c03;
+				C[c1] = c10;
+				C[c1 + 1] = c11;
+				C[c1 + 2] = c12;
+				C[c1 + 3] = c13;
+				C[c2] = c20;
+				C[c2 + 1] = c21;
+				C[c2 + 2] = c22;
+				C[c2 + 3] = c23;
+				C[c3] = c30;
+				C[c3 + 1] = c31;
+				C[c3 + 2] = c32;
+				C[c3 + 3] = c33;
 			}
-			C[c0] = c00;
-			C[c0 + 1] = c01;
-			C[c0 + 2] = c02;
-			C[c1] = c10;
-			C[c1 + 1] = c11;
-			C[c1 + 2] = c12;
-			C[c2] = c20;
-			C[c2 + 1] = c21;
-			C[c2 + 2] = c22;
-		}
+
     return;
 }
 
@@ -246,7 +270,7 @@ int mydgetrf_block(double *A, int *ipiv, int n, int b)
 	double max, sum;
 	double *tempv = (double*)malloc(sizeof(double) * n);
 
-	for (ib = 0; ib < n; ib += b)
+	for (ib = 0; ib < (n - 1); ib += b)
 	{
 		for (i = ib; i < ib + b && i < n; i++)
 		{
